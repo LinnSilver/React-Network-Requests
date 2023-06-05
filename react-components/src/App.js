21a20c9c848367308c06f64f378e2c0c495eefaf
LinnSilver <baralinn@gmail.com>
2023-06-05 11:06:44 +0000

    Add HTTPHooks

--- react-components/src/App.js
+++ react-components/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import HTTPHooks from './components/HTTPHooks';
 import HTTPPost from './components/HTTPPost';
 import HTTPRequests from "./components/HTTPRequests";
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <HTTPRequests />
       <HTTPPost />
+      <HTTPHooks />
     </div>
   );
 }
